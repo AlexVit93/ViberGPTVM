1b951940f2c50d71004c704708ad16f65007d89c
Antuane_Specione <boosla@gmail.com>
2023-12-06 16:38:04 +0300

    refactor:re-write code for viberbot using

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import logging
-from aiogram import Bot, Dispatcher, types
 import g4f
-from aiogram.utils import executor
-from config import API_TOKEN
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages import TextMessage
+from viberbot.api.viber_requests import ViberMessageRequest
+from config import VIBER_AUTH_TOKEN
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-# Словарь для хранения истории разговоров
+# Настройка бота Viber
+viber = Api(BotConfiguration(
+    name='ChatGPTPersonalBotbyVM',
+    avatar='https://dl-media.viber.com/1/share/2/long/vibes/icon/image/0x0/7a08/3c87d21eceedb833743a81c19b74cea8c1c3e4ef66e7c86b71d82d16c1147a08.jpg',
+    auth_token=VIBER_AUTH_TOKEN
+))
+
 conversation_history = {}
 
-# Функция для обрезки истории разговора
 def trim_history(history, max_length=4096):
     current_length = sum(len(message["content"]) for message in history)
     while history and current_length > max_length:
@@ -19,47 +24,39 @@ def trim_history(history, max_length=4096):
         current_length -= len(removed_message["content"])
     return history
 
+def message_received_callback(viber_request):
+    if isinstance(viber_request, ViberMessageRequest):
+        user_id = viber_request.sender.id
+        user_input = viber_request.message.text
 
-@dp.message_handler(commands=['clear'])
-async def process_clear_command(message: types.Message):
-    user_id = message.from_user.id
-    conversation_history[user_id] = []
-    await message.reply("История диалога очищена.")
+        viber.send_messages(user_id, [TextMessage(text="Подождите пожалуйста, я обрабатываю ваш запрос...")])
 
-# Обработчик для каждого нового сообщения
-@dp.message_handler()
-async def send_welcome(message: types.Message):
-    user_id = message.from_user.id
-    user_input = message.text
+        if user_id not in conversation_history:
+            conversation_history[user_id] = []
 
-    await message.reply("Подождите пожалуйста, я обрабатываю ваш запрос...")
+        conversation_history[user_id].append({"role": "user", "content": user_input})
+        conversation_history[user_id] = trim_history(conversation_history[user_id])
 
-    if user_id not in conversation_history:
-        conversation_history[user_id] = []
+        chat_history = conversation_history[user_id]
 
-    conversation_history[user_id].append({"role": "user", "content": user_input})
-    conversation_history[user_id] = trim_history(conversation_history[user_id])
+        try:
+            response = g4f.ChatCompletion.create(
+                model=g4f.models.default,
+                messages=chat_history,
+                provider=g4f.Provider.GeekGpt,
+            )
+            chat_gpt_response = response
+        except Exception as e:
+            print(f"{g4f.Provider.GeekGpt.__name__}:", e)
+            chat_gpt_response = "Извините, произошла ошибка."
 
-    chat_history = conversation_history[user_id]
+        conversation_history[user_id].append({"role": "assistant", "content": chat_gpt_response})
+        print(conversation_history)
+        length = sum(len(message["content"]) for message in conversation_history[user_id])
+        print(length)
 
-    try:
-        response = await g4f.ChatCompletion.create_async(
-            model=g4f.models.default,
-            messages=chat_history,
-            provider=g4f.Provider.GeekGpt,
-        )
-        chat_gpt_response = response
-    except Exception as e:
-        print(f"{g4f.Provider.GeekGpt.__name__}:", e)
-        chat_gpt_response = "Извините, произошла ошибка."
+        viber.send_messages(user_id, [TextMessage(text=chat_gpt_response)])
 
-    conversation_history[user_id].append({"role": "assistant", "content": chat_gpt_response})
-    print(conversation_history)
-    length = sum(len(message["content"]) for message in conversation_history[user_id])
-    print(length)
-    await message.answer(chat_gpt_response)
-
-
-# Запуск бота
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    viber.set_webhook('https://your.server.com/')
+
