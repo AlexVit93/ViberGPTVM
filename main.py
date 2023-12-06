10ee1f4c192adff8c703441c9f98d2d00374bd5f
Antuane_Specione <boosla@gmail.com>
2023-12-07 01:37:34 +0300

    fix:update code, return gpt4 work function

--- main.py
+++ main.py
@@ -34,14 +34,54 @@ def incoming():
         viber.send_messages(viber_request.user.id, [
             TextMessage(text="Thanks for subscribing!")
         ])
-    # Повторяющийся блок удалён
+
     elif isinstance(viber_request, ViberFailedRequest):
         pass
     
     return Response(status=200)
 
-# Остальная часть кода без изменений...
+conversation_history = {}
+
+def trim_history(history, max_length=4096):
+    current_length = sum(len(message["content"]) for message in history)
+    while history and current_length > max_length:
+        removed_message = history.pop(0)
+        current_length -= len(removed_message["content"])
+    return history
+
+def message_received_callback(viber_request):
+    if isinstance(viber_request, ViberMessageRequest):
+        user_id = viber_request.sender.id
+        user_input = viber_request.message.text
+
+        viber.send_messages(user_id, [TextMessage(text="Подождите пожалуйста, я обрабатываю ваш запрос...")])
+
+        if user_id not in conversation_history:
+            conversation_history[user_id] = []
+
+        conversation_history[user_id].append({"role": "user", "content": user_input})
+        conversation_history[user_id] = trim_history(conversation_history[user_id])
+
+        chat_history = conversation_history[user_id]
+
+        try:
+            response = g4f.ChatCompletion.create(
+                model=g4f.models.default,
+                messages=chat_history,
+                provider=g4f.Provider.GeekGpt,
+            )
+            chat_gpt_response = response
+        except Exception as e:
+            print(f"{g4f.Provider.GeekGpt.__name__}:", e)
+            chat_gpt_response = "Извините, произошла ошибка."
+
+        conversation_history[user_id].append({"role": "assistant", "content": chat_gpt_response})
+        print(conversation_history)
+        length = sum(len(message["content"]) for message in conversation_history[user_id])
+        print(length)
+
+        viber.send_messages(user_id, [TextMessage(text=chat_gpt_response)])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)), debug=True)
-    # Здесь должен быть код для установки webhook, если вы не делали это вручную через платформу Viber
+
